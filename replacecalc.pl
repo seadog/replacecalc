0b7e3f7e82a042918038292a3ea41d76ab6093a3
Andrew Etches <andrew.etches@dur.ac.uk>
2012-03-29 13:03:45 +0100

    Using google to do the calculations

--- replacecalc.pl
+++ replacecalc.pl
@@ -2,6 +2,9 @@ use strict;
 use vars qw($VERSION %IRSSI);
 
 use Irssi;
+use URI::Escape;
+use LWP::UserAgent;
+use JSON;
 
 $VERSION = '1.0';
 %IRSSI = (
@@ -11,12 +14,23 @@ $VERSION = '1.0';
     license => "MIT",
 );
 
+my $agent = LWP::UserAgent->new(
+    agent => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.142 Safari/535.19",
+);
+
 sub replace {
     my ($data, $server, $witem) = @_;
     return unless $witem;
 
     while($data =~ /\@\{([^\}]+)\}\@/){
-        my $result = eval($1);
+        my $url = "http://www.google.com/ig/calculator?num=1&q=".uri_escape($1);
+        my $response = $agent->get($url);
+        my $json = new JSON;
+        my $text = $response->content;
+        $text =~ s/([a-z]+)\:/\"$1\"\:/g;
+        my $jsontext = $json->decode($text);
+
+        my $result = $jsontext->{rhs};
         $data =~ s/\@\{[^\}]+\}\@/$result/;
     }
 
